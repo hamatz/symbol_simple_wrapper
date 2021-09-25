9a58dabc437eaba5e4f3c3106cb5c4bc283cfa20
hamatz <hamatsu.m@googlemail.com>
2021-09-25 22:58:46 +0900

    モザイク発行サンプルにコメント追加

--- example4.py
+++ example4.py
@@ -6,24 +6,31 @@ from utils.wallet import SimpleWallet
 import secrets
 
 def main(network_id, pass_phrase, key_file_name):
+	'''
+	モザイクの新規発行と初期供給総量の定義
+	'''
     wallet = SimpleWallet(network_id, key_file_name, pass_phrase)
     address = wallet.get_my_address()
-    print('Here is your address: ' , address)
     pubkey = wallet.get_my_pubkey_string()
-    print('Here is your pubkey: ' , pubkey)
 
     deadline = (int((datetime.datetime.today() + datetime.timedelta(hours=2)).timestamp()) - 1616694977) * 1000
     fee = 1000000
+    # モザイクの新規発行にあたっての各種定義
     duration = 365
     flags = 'transferable restrictable supply_mutable'
     divisibility = 2
     result = wallet.send_mosaic_def_transacton(deadline, fee, duration, flags, divisibility)
     print(result[0])
 
+	# モザイクの発行総量を定義する。全てが発行者所有となる
     delta = 100000
     action = 'increase'
     result2 = wallet.send_mosaic_supply_change_tx(deadline, fee, delta, action)
 
+	'''
+	自分のアカウント情報の確認。通常、このタイミングではトランザクションの反映は行われておらず
+	新規発行分は見えない
+	'''
     account_info = wallet.check_my_account_info_with_address()
     a_info = account_info[0]['account']
     print(json.dumps(account_info, indent=2))
