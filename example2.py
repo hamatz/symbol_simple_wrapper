a45b58519758fd39b8467d19f2f3eefc3eaecba2
Makoto HAMATSU <hamatsu.m@googlemail.com>
2021-09-28 21:15:57 +0900

    コメントの追記

--- example2.py
+++ example2.py
@@ -4,36 +4,44 @@ import json
 from utils.wallet import SimpleWallet
 
 def main(network_id, pass_phrase, key_file_name):
-	'''
-	1. 自分のアドレス、公開鍵の確認
-	2. 自分のアカウント情報（残高等）の確認
-	3. 受信した最新100までのトランザクションの確認
-	4. 現在のトランザクションfeeの確認
-	5. Thanks Meterへの「+1」メッセージの送信（自分宛）
-	6. 送信トランザクション（最新100）の確認
-	'''
+    '''
+    1. 自分のアドレス、公開鍵の確認
+    2. 自分のアカウント情報（残高等）の確認
+    3. 受信した最新100までのトランザクションの確認
+    4. 現在のトランザクションfeeの確認
+    5. Thanks Meterへの「+1」メッセージの送信（自分宛）
+    6. 送信トランザクション（最新100）の確認
+    '''
+    # 初期化
     wallet = SimpleWallet(network_id, key_file_name, pass_phrase)
+    # 1. 自分のアドレス、公開鍵の確認
     address = wallet.get_my_address()
     print('Here is your address: ' , address)
     pubkey = wallet.get_my_pubkey_string()
     print('Here is your pubkey: ' , pubkey)
+    # 2. 自分のアカウント情報（残高等）の確認
     print('Here is your account info')
     account_info = wallet.check_my_account_info_with_address()
     print(json.dumps(account_info, indent=2))
+    # 3. 受信した最新100までのトランザクションの確認
     print('Here is your received transactions (latest 100)')
     txs = wallet.get_received_transactions_with_address(address, 100)
     print(json.dumps(txs, indent=2))
+    # 4. 現在のトランザクションfeeの確認
     print('Here is current transaction fee')
     tx_fee = wallet.get_tx_fee_info()
     print(json.dumps(tx_fee, indent=2))
 
     deadline = (int((datetime.datetime.today() + datetime.timedelta(hours=2)).timestamp()) - 1616694977) * 1000
     fee = 1000000
+    # 5. Thanks Meterへの「+1」メッセージの送信（自分宛）
     result = wallet.send_thanks_transacton(deadline, fee, address)
     print(result[0])
+    # 送信したトランザクションの状態を確認
     tx_status = wallet.get_transaction_status(str(result[1]))
     print(json.dumps(tx_status, indent=2))
 
+    # 6. 送信トランザクション（最新100）の確認
     sent_transactions = wallet.get_sent_transactions(pubkey, 100)
     print('Here is your sent transactions (latest 100)')
     print(json.dumps(sent_transactions, indent=2))
