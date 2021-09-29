b0432ac3a55d0526ff36b51ab004d81d4e10efb7
Makoto HAMATSU <hamatsu.m@googlemail.com>
2021-09-29 21:49:28 +0900

    コメントの追加

--- example3.py
+++ example3.py
@@ -10,15 +10,14 @@ def main(network_id, pass_phrase, key_file_name):
     pubkey = wallet.get_my_pubkey_string()
     print('Here is your pubkey: ' , pubkey)
 
+    #  base32ToHexAddress変換
     h_address = wallet.get_hex_address(address)
     print(h_address.decode())
 
+    #  base32ToHexAddress逆変換
     uh_address = wallet.get_unhexed_address(h_address)
     print(uh_address)
 
-    account_info = wallet.check_my_account_info_with_address()
-    a_info = account_info[0]['account']
-    print(json.dumps(account_info, indent=2))
 
 if __name__ == '__main__':
     args = sys.argv
