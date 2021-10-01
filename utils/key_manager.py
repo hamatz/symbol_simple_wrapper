42a8e90da588460a9e74620261612533ad9e4ee6
Makoto HAMATSU <hamatsu.m@googlemail.com>
2021-10-01 23:12:53 +0900

    インデントの調整

--- utils/key_manager.py
+++ utils/key_manager.py
@@ -5,37 +5,37 @@ from symbolchain.core.facade.SymFacade import SymFacade
 
 class KeyManager:
 
-	def __init__(self, facade, dir_name='', pass_phrase= None, privatekey_path = None):
-		print('Initializing KeyManager...')
-		self._facade = facade
-		self._p_storage = PrivateKeyStorage(dir_name, pass_phrase)
+    def __init__(self, facade, dir_name='', pass_phrase= None, privatekey_path = None):
+        print('Initializing KeyManager...')
+	self._facade = facade
+	self._p_storage = PrivateKeyStorage(dir_name, pass_phrase)
 
-		if privatekey_path:
-			self._private_key = self._p_storage.load(privatekey_path)
-		else:
-			self._private_key = PrivateKey.random()
+	if privatekey_path:
+	    self._private_key = self._p_storage.load(privatekey_path)
+	else:
+	    self._private_key = PrivateKey.random()
 
-		self._keypair = SymFacade.KeyPair(self._private_key)
-		self._public_key = self._keypair.public_key
+	self._keypair = SymFacade.KeyPair(self._private_key)
+	self._public_key = self._keypair.public_key
 
-	def get_my_pubkey(self):
-		return self._public_key
+    def get_my_pubkey(self):
+        return self._public_key
 
-	def export_my_key(self, key_file_name):
-		"""
-		秘密鍵をファイルとして保存。ファイル保存する際はpem形式。初期化時にpass_phraseを指定していたらそれを使って保護する
-		"""
-		self._p_storage.save(key_file_name, self._private_key)
+    def export_my_key(self, key_file_name):
+	"""
+	秘密鍵をファイルとして保存。ファイル保存する際はpem形式。初期化時にpass_phraseを指定していたらそれを使って保護する
+	"""
+	self._p_storage.save(key_file_name, self._private_key)
 
-	def sign_tx(self, transaction):
-		return self._facade.sign_transaction(self._keypair, transaction)
+    def sign_tx(self, transaction):
+        return self._facade.sign_transaction(self._keypair, transaction)
 
-	def verify_tx(self, transaction, signature):
-		return self._facade.verify_transaction(transaction, signature)
+    def verify_tx(self, transaction, signature):
+	return self._facade.verify_transaction(transaction, signature)
 
-	def compute_signature(self, target):
-		return self._keypair.sign(target)
+    def compute_signature(self, target):
+	return self._keypair.sign(target)
 
-	def verify_signature(self, target, signature, pubkey):
-		verifier = SymFacade.Verifier(pubkey)
-		return verifier.verify(target, signature)
+    def verify_signature(self, target, signature, pubkey):
+	verifier = SymFacade.Verifier(pubkey)
+	return verifier.verify(target, signature)
